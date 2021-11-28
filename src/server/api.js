aaa28c35faa06e78f98563bbe95ad429652bf9aa
avery <averywald.dev@gmail.com>
2021-11-28 12:18:19 -0600

    reorganized file structure;

--- src/server/api.js
+++ src/server/api.js
@@ -1,23 +1,16 @@
 const cors = require('cors'); // CORS middleware
 const dotenv = require('dotenv'); // global vars
 const helmet = require('helmet'); // header security
-let mongoClient = require('mongodb').MongoClient; // MongoDB hook
+
 const morgan = require('morgan'); // request logger
-
-// set up express app
-const express = require('express');
-let app = express();
-
-// set up the web server
-const httpServer = require('http').createServer(app);
-// set up socket io and attach it to the web server
-const io = require('socket.io')(httpServer);
+const express = require('express'); // set up express app
 
 // allow for .env global imports
 dotenv.config();
 // specify ports to use
 const port = process.env.ALT_PORT;
 
+let app = express(); // init express web api
 app.use([
     helmet(), // attach middleware
     morgan('dev'), // attach request logger
@@ -25,8 +18,14 @@ app.use([
 ]);
 app.use(express.static('public')); // set up static file serving root
 
-// serve app core
-app.get('/', (res) => {
+// NOTE
+// https://expressjs.com/en/api.html:
+// Once set, the value of app.locals properties persist throughout the life of the application, 
+// in contrast with res.locals properties that are valid only for the lifetime of the request.
+app.locals.title = 'pixlr';
+
+// serve app root
+app.get('/', (res, req, next) => {
 
     // specify options to pass with served file
     const options = {
@@ -45,6 +44,48 @@ app.get('/', (res) => {
     });
     
 });
+
+// have the app listen to traffic at the set port
+app.listen(port, () => {
+
+    // log port listening message to server console
+    console.log(`listening at http://localhost:${port}`);
+
+});
+
+// socket.io stuff ==========================================================================
+
+import { app } from './api.js';
+
+const httpServer = require('http').createServer(app); // set up the web server via the express API
+const io = require('socket.io')(httpServer); // set up socket io and attach it to the web server
+const mongoClient = require('mongodb').MongoClient; // MongoDB hook
+
+// a client connects
+io.on('connection', socket => {
+
+    // send the client the current master copy
+    updateClient();
+
+    // client clicks somewhere on the canvas
+    socket.on('click', data => {
+        // update the master canvas copy
+        updateMaster(data);
+
+        // send the updates to the client
+        updateClient();
+    });
+
+});
+
+// 
+function updateClient() {
+    // get the canvas master copy
+    retrieve().then(data => {
+        // send to the client
+        socket.emit('update', data);
+    });
+}
 
 async function retrieve() {
 
@@ -89,37 +130,3 @@ async function updateMaster(data) {
     }
 
 }
-
-// a client connects
-io.on('connection', socket => {
-
-    // 
-    function updateClient() {
-        // get the canvas master copy
-        retrieve().then(data => {
-            // send to the client
-            socket.emit('update', data);
-        });
-    }
-
-    // send the client the current master copy
-    updateClient();
-
-    // client clicks somewhere on the canvas
-    socket.on('click', data => {
-        // update the master canvas copy
-        updateMaster(data);
-
-        // send the updates to the client
-        updateClient();
-    });
-
-});
-
-// have the app listen to traffic at the set port
-httpServer.listen(port, () => {
-
-    // log port listening message to server console
-    console.log(`listening at http://localhost:${port}`);
-
-});
